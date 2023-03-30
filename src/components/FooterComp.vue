05d6e49484b2b6d66d061f9134ac0f5db846dd4a
72fcosta <inbox5372@gmail.com>
2023-03-30 06:42:34 -0300

    update 1 file and delete 8 files

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import IconLogo from '@/components/icons/IconLogo.vue'
+</script>
+
+<template>
+   <footer class="footer-t flex h-[42px] items-center justify-center">
+      <div class="border-t-t"></div>
+      <div class="flex items-center gap-x-1 text-xs leading-none">
+         <span>2023</span>
+         <span>©</span>
+         <span class="ml-[2px] mt-[1px] border-r border-white/10 pr-[4px]">
+            <IconLogo class="w-[70px] fill-[#6287BB]/50" />
+         </span>
+         <span class="pb-[1px]">Desenvolvedor Web</span>
+      </div>
+   </footer>
+</template>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import IconLogo from '@/components/icons/IconLogo.vue'
+import FooterComp from '@/components/FooterComp.vue'
+</script>
+
+<template>
+   <NavComp class="hidden xl:flex" />
+
+   <main class="container flex max-w-screen-xl grow p-4 xl:hidden">
+      <div class="card-t w-full max-w-sm place-self-center">
+         <div class="card-body gap-y-8">
+            <div>
+               <div class="flex items-center gap-x-1">
+                  <IconAlert />
+                  <p>Mínimo de 1280px para acessar</p>
+               </div>
+               <hr class="mt-2" />
+            </div>
+
+            <div class="mb-2 flex justify-center">
+               <IconLogo class="w-56 fill-neutral" />
+            </div>
+
+            <a href="#" class="btn-t gap-x-2">
+               <span class="text-base">Sair</span>
+            </a>
+         </div>
+      </div>
+   </main>
+
+   <main class="container hidden max-w-screen-xl grow p-4 xl:flex">
+      <router-view />
+   </main>
+
+   <FooterComp class="hidden xl:flex" />
+</template>
