dcb4401647e87acc6f146711291aa0f17810503f
72fcosta <inbox5372@gmail.com>
2023-03-30 11:02:59 -0300

    update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-   <main class="w-full bg-[#060e2c]">
-      <h1 class="w-full bg-[#132865] p-6">SANDBOX</h1>
+   <main class="flex w-full flex-col bg-[#060e2c]">
+      <h1 class="flex w-full bg-[#132865] p-6">SANDBOX</h1>
+      <div class="grow px-8 py-5">_</div>
    </main>
 </template>
