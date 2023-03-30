704c4cf27a5be079536370f93816965eaafef36c
72fcosta <inbox5372@gmail.com>
2023-03-30 10:27:06 -0300

    update 5 files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-   <main class='w-full bg-[#060e2c] rounded-lg overflow-auto'>
-      <h1 class='w-full bg-[#132865] p-6'>SANDBOX</h1>
+   <main class="w-full bg-[#060e2c]">
+      <h1 class="w-full bg-[#132865] p-6">SANDBOX</h1>
    </main>
 </template>
