e0e1f6f3099b263e1b95cbba0df874dbe4a346a7
72fcosta <inbox5372@gmail.com>
2023-03-30 07:20:01 -0300

    update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-   <main class='w-full bg-red-400 rounded-lg'>
+   <main class='w-full bg-[#3D3199] rounded-lg'>
       <h1 class='w-full bg-white/10 rounded-lg p-6'>Sandbox</h1>
    </main>
 </template>
