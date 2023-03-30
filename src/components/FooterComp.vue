d0507869fc3c5f3ad4c1264d9dae874e61f53f59
72fcosta <inbox5372@gmail.com>
2023-03-30 07:03:22 -0300

    update index.html and FooterComp.vue

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,13 +3,12 @@ import IconLogo from '@/components/icons/IconLogo.vue'
 </script>
 
 <template>
-   <footer class="footer-t flex h-[42px] items-center justify-center">
-      <div class="border-t-t"></div>
+   <footer class="text-white/50 flex h-[42px] items-center justify-center bg-[#0B173A]">
       <div class="flex items-center gap-x-1 text-xs leading-none">
          <span>2023</span>
          <span>©</span>
          <span class="ml-[2px] mt-[1px] border-r border-white/10 pr-[4px]">
-            <IconLogo class="w-[70px] fill-[#6287BB]/50" />
+            <IconLogo class="w-[70px]" />
          </span>
          <span class="pb-[1px]">Desenvolvedor Web</span>
       </div>
